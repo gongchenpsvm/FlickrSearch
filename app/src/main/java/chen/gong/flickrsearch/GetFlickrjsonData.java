3efdff0906b777c87624899a7716dfeb269bdc2d
gongchen <gongchen@usc.edu>
2017-01-07 12:22:13 -0800

    Implement GetFlickrjsonData to parse JSON downloaded by GetRawData, catches not complete yet

---
+++ app/src/main/java/chen/gong/flickrsearch/GetFlickrjsonData.java
@@ -0,0 +1,85 @@
+package chen.gong.flickrsearch;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gongchen on 1/7/17.
+ */
+
+class GetFlickrjsonData implements GetRawData.OnDownloadComplete {
+    private static final String TAG = "GetFlickrjsonData";
+
+    private List<Photo> mPhotoList = null;
+    private String mBaseURL;
+    private String mLanguage;
+    private boolean mMatchAll;
+    private final OnDataAvailable mCallBack;
+
+    interface OnDataAvailable {
+        void onDataAvailable(List<Photo> data, DownloadStatus status);
+    }
+
+    public GetFlickrjsonData(OnDataAvailable callBack, String baseURL
+            , String language, boolean matchAll) {
+        Log.d(TAG, "GetFlickrjsonData called");
+        mBaseURL = baseURL;
+        mLanguage = language;
+        mMatchAll = matchAll;
+        mCallBack = callBack;
+    }
+
+    void executeOnSameThread(String searchCriteria) {
+        Log.d(TAG, "executeOnSameThread starts");
+        String destinationUri = createUri(searchCriteria, mLanguage, mMatchAll);
+
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute(destinationUri);
+        Log.d(TAG, "executeOnSameThread ends");
+    }
+
+    private String createUri(String searchCriteria, String lang, boolean matchAll){
+        Log.d(TAG, "createUri starts");
+
+        return Uri.parse(mBaseURL).buildUpon()
+                .appendQueryParameter("tags", searchCriteria)
+                .appendQueryParameter("tagmode", matchAll ? "ALL" : "ANY")
+                .appendQueryParameter("lang", lang)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .build().toString();
+    }
+
+    @Override
+    public void onDownloadComplete(String data, DownloadStatus status) {
+        Log.d(TAG, "onDownloadComplete starts. Status = " + status);
+        if (status == DownloadStatus.OK){//If ok, process
+            mPhotoList = new ArrayList<>();
+
+            try {
+                JSONObject jsonData = new JSONObject(data);
+                JSONArray itemsArray = jsonData.getJSONArray("items");
+
+                for (int i = 0; i < itemsArray.length(); i++){
+                    JSONObject jsonPhoto = itemsArray.getJSONObject(i);
+                    String title = jsonPhoto.getString("title");
+                    String author = jsonPhoto.getString("author");
+                    String authorId = jsonPhoto.getString("author_id");
+                    String tags = jsonPhoto.getString("tags");
+
+                    JSONObject jsonMedia = jsonPhoto.getJSONObject("media");
+                    String photoUrl = jsonMedia.getString("m");
+
+                    String link = photoUrl.replaceFirst("_m.", "_b.");
+                }
+            }
+        }
+
+    }
+}
